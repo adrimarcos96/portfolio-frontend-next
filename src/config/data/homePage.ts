b3e7098d3cc6942b40461028259730aa38b4e8e7
Adrián Marcos <adrimarcos1996@gmail.com>
2023-09-29 01:16:29 -0400

    Fixing linter

--- src/config/data/homePage.ts
+++ src/config/data/homePage.ts
@@ -1,13 +1,13 @@
 const aboutMe: string[] = [
-  `I am Adrián Marcos Quintana, I am 26 years old. I am Informatic Engineer.`,
-  `I hope to become a successful senior web developer and continue to grow more as a developer every day.`,
-  `I am open to acquiring new knowledge.`
+  'I am Adrián Marcos Quintana, I am 26 years old. I am Informatic Engineer.',
+  'I hope to become a successful senior web developer and continue to grow more as a developer every day.',
+  'I am open to acquiring new knowledge.'
 ]
 
 const experience: string[] = [
-  `September, 2020 - Januany, 2023: Pogramming teacher at University of Matanzas, Cuba`,
-  `July, 2021 - present: Full-stack developer at 321 Ignition`,
-  `Adrian Marcos portfolio`
+  'September, 2020 - Januany, 2023: Pogramming teacher at University of Matanzas, Cuba',
+  'July, 2021 - present: Full-stack developer at 321 Ignition',
+  'Adrian Marcos portfolio'
 ]
 
 export const homePageData = {
